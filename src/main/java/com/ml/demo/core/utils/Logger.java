3cd51a88543e570b87b47f994f5083d8a383150b
mlucman <matija.lucman@infobip.com>
2018-01-05 21:20:52 +0100

    Gradle upgrade.

--- src/main/java/com/ml/demo/core/utils/Logger.java
+++ src/main/java/com/ml/demo/core/utils/Logger.java
@@ -1,8 +1,11 @@
 package com.ml.demo.core.utils;
 
+import com.cucumber.listener.Reporter;
+
 public class Logger {
 
     public static void log(String logText) {
         System.out.println(logText);
+        Reporter.addStepLog(logText);
     }
 }
